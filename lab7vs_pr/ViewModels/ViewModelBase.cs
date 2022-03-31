2509895be41a34f6a0d26e68947aa34d98605683
Victerst <78637179+Victerst@users.noreply.github.com>
2022-04-01 00:02:38 +0700

    add lab

---
+++ lab7vs_pr/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab7vs_pr.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
